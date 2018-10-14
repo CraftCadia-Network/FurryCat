cbc80f2ebb92db1f614fdb7d9b260f0b24f07ec6
Samuel Taber <admin@cadianetwork.com>
2018-10-14 11:38:32 -0400

    Bunch Of Fun Things
    Oh Man.

--- src/me/dinosphere/FurryCat/FurryCat.java
+++ src/me/dinosphere/FurryCat/FurryCat.java
@@ -1,9 +1,35 @@
 package me.dinosphere.FurryCat;
 
+import java.util.logging.Logger;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.dinosphere.FurryCat.oxygenHandler.oxygenLevel;
 
 public class FurryCat extends JavaPlugin{
 
+
 	
+	public static Logger logger = Logger.getLogger("Minecraft");
+	public static FurryCat plugin;
+	
+	// Console Message (When Turned Off)	
+	@Override
+	public void onDisable() {
+		
+		PluginDescriptionFile pdfFile = this.getDescription();
+		FurryCat.logger.info(pdfFile.getName() + " has been disabled.");
+		
+	}
+	// Console Message (When Turned On)
+	@Override
+	public void onEnable() {
+		
+		PluginDescriptionFile pdfFile = this.getDescription();
+		FurryCat.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() +  "has been enabled. Starting CadiaCore..");
+		oxygenLevel.dinosaur();
+	}
+		
+		
 	
 }
---
+++ src/me/dinosphere/FurryCat/oxygenHandler/oxygenLevel.java
@@ -0,0 +1,45 @@
+package me.dinosphere.FurryCat.oxygenHandler;
+
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class oxygenLevel extends JavaPlugin{
+
+ 
+	
+
+
+		@SuppressWarnings("null")
+		public static void dinosaur() {		
+			
+			final CommandSender sender = null;
+			final Command command;
+			final String label;
+			final String[] args;
+			final Player player = (Player) sender;
+			
+			try {
+		        while (true) {
+		            player.setRemainingAir(-1);
+		            Thread.sleep(600000);
+		        }
+		    } catch (InterruptedException e) {
+		        e.printStackTrace();
+		    }
+				
+			}
+}
+		
+
+			
+		
+
