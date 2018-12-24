a4d34e86e0961b82fc97e08f88b384810ae0916a
Samuel Taber <admin@cadianetwork.com>
2018-12-24 12:35:56 -0500

    Built Jar. Added Heal Command
    Heal yourself.

--- src/me/dinosphere/FurryCat/FurryCat.java
+++ src/me/dinosphere/FurryCat/FurryCat.java
@@ -2,16 +2,14 @@ package me.dinosphere.FurryCat;
 
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.dinosphere.FurryCat.commands.HealCommand;
 
 
 
@@ -40,9 +38,7 @@ public class FurryCat extends JavaPlugin implements Listener{
 	}
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String args[]) {
 		
-		
-		Player player = (Player) sender;
-	
+		this.getCommand("heal").setExecutor((CommandExecutor)new HealCommand());
 		
 		return false;
 	}
